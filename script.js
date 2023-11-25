1182fdc1f9f7fd12edf2508e1ca54cafc6a18a67
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-11-25 12:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+  function focusNext(currentInput) {
+            const maxLength = parseInt(currentInput.getAttribute('maxlength'));
+            const nextInput = currentInput.nextElementSibling;
+
+            if (currentInput.value.length === maxLength && nextInput) {
+                nextInput.focus();
+            }
+        }
+
+        function focusPrev(currentInput, event) {
+            const prevInput = currentInput.previousElementSibling;
+
+            if (event.key === 'Backspace' && !currentInput.value && prevInput) {
+                prevInput.focus();
+            }
+        }
